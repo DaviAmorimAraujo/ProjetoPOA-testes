768bad0359ce9e19f742e0bea9e6872df34bca35
DaviAmorimAraujo <davi.araujo@ucsal.edu.br>
2026-05-26 11:40:57 -0300

    Update RelatorioService.java

--- src/service/RelatorioService.java
+++ src/service/RelatorioService.java
@@ -1,9 +1,17 @@
 package service;
 
+import singleton.RelatorioSingleton;
+
 public class RelatorioService {
 
-	public static void main(String[] args) {
+    // obs: deve ser chamado somente dps do thread.join() na Main
+    public void exibirRelatorio() {
+        RelatorioSingleton r = RelatorioSingleton.getInstance();
 
-	}
-
+        System.out.println("\n=== RELATORIO DE VENDAS ===");
+        System.out.printf("Total vendido : R$ %,.2f%n", r.getTotalVendas());
+        System.out.printf("Total de itens: %d unidades%n", r.getTotalItens());
+        System.out.printf("Mais vendido  : %s%n", r.getMaisVendido());
+        System.out.println("===========================\n");
+    }
 }
